00a4bcd9c92862431e363cc444c8f1f022dd8a1b
Octavia <octaviapalmer@hotmail.com>
2017-10-18 11:35:31 +1300

    users are able to enter a new subreddit

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-let LoadSubreddit = ({dispatch}) => (
-  <button
-    onClick={() => dispatch(fetchPosts('newzealand'))}
-    >Fetch Posts</button>
-)
+// change to stateful component
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      newSubreddit: ''
+    }
+    this.userInput = this.userInput.bind(this)
+  }
+
+  userInput (e) {
+    this.setState({newSubreddit: e.target.value})
+  }
+
+  render () {
+    return (
+      <div>
+          <input type='text' name='newSubreddit' placeholder='Post titles' value={this.state.newSubreddit} onChange={this.userInput}/>
+          <button
+            onClick={() => this.props.dispatch(fetchPosts(this.state.newSubreddit))}
+            >Fetch Posts</button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
